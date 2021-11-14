b81880bf2aebde4ead3aeb2a54dba1f0321b5f40
Alex Schneidman <alexmarkschneidman@gmail.com>
2021-11-14 12:50:12 -0500

    progress on integrating security

---
+++ lab7/rez_diarization.py
@@ -0,0 +1,104 @@
+from resemblyzer import preprocess_wav, VoiceEncoder
+from demo_utils import *
+from pathlib import Path
+import numpy as np
+from random_projections import enroll, query
+
+
+
+# DEMO 02: we'll show how this similarity measure can be used to perform speaker diarization
+# (telling who is speaking when in a recording).
+
+
+## Get reference audios
+# Load the interview audio from disk
+# Source for the interview: https://www.youtube.com/watch?v=X2zqiX6yL3I
+wav_fpath = "X2zqiX6yL3I.mp3"
+wav = preprocess_wav(wav_fpath)
+
+# Cut some segments from single speakers as reference audio
+segments = [[0, 5.5], [6.5, 12], [17, 25]]
+speaker_names = ["Kyle Gass", "Sean Evans", "Jack Black"]
+speaker_wavs = [wav[int(s[0] * sampling_rate):int(s[1] * sampling_rate)] for s in segments]
+  
+encoder = VoiceEncoder("cpu")
+speaker_embeds = [encoder.embed_utterance(speaker_wav) for speaker_wav in speaker_wavs]
+
+
+# enroll users
+speaker_passwords = [str(np.random.rand(1)[0].tolist()) for _ in range(len(speaker_wavs))]
+print(f"Passwords: {speaker_passwords}")
+
+enrollments = []
+for i in range(len(speaker_passwords)):
+    name, password, feature = speaker_names[i], speaker_passwords[i], speaker_embeds[i]
+
+    enroll(name, password, feature, enrollments)
+
+
+# Test that the templates match in the secure space
+for name, speaker_embed in zip(speaker_names, speaker_embeds):
+    print(f"Actual: {name}, Pred: {query(speaker_embed, enrollments).name}")
+
+    
+## Compare speaker embeds to the continuous embedding of the interview
+# Derive a continuous embedding of the interview. We put a rate of 16, meaning that an 
+# embedding is generated every 0.0625 seconds. It is good to have a higher rate for speaker 
+# diarization, but it is not so useful for when you only need a summary embedding of the 
+# entire utterance. A rate of 2 would have been enough, but 16 is nice for the sake of the 
+# demonstration. 
+# We'll exceptionally force to run this on CPU, because it uses a lot of RAM and most GPUs 
+# won't have enough. There's a speed drawback, but it remains reasonable.
+
+print("Running the continuous embedding on cpu, this might take a while...")
+
+wav = wav[:100]
+
+_, cont_embeds, wav_splits = encoder.embed_utterance(wav, return_partials=True, rate=16)
+
+
+predicted_speakers_secure = []
+predicted_speakers_not_secure = []
+# Query each feature vector
+for embedding in cont_embeds:
+    lowest_e = query(embedding, enrollments)
+    predicted_speakers_secure.append(lowest_e.name)
+
+    best_sim = float("inf")
+    best_name = None
+    for name, speaker_embed in zip(speaker_names, speaker_embeds):
+        sim = embedding @ speaker_embed
+        
+        print(f"Non-Secure: Name {name}, Dist: {sim}")
+        if sim < best_sim:
+            best_sim = sim
+            best_name = name
+
+
+    predicted_speakers_not_secure.append(best_name)
+
+accuracy = 0.0
+# compute accuracy relative to non secure version
+for pred_secure, pred_not_secure in zip(predicted_speakers_secure, predicted_speakers_not_secure):
+    accuracy += float(pred_secure == pred_not_secure)
+
+accuracy /= len(predicted_speakers_not_secure)
+
+print(f"Accuracy: {accuracy}")
+
+predicted_speakers_not_secure = [s + '\n' for s in predicted_speakers_not_secure]
+predicted_speakers_secure = [s + '\n' for s in predicted_speakers_secure]
+
+with open('secure_names_output.txt', 'w') as f:
+    f.writelines(predicted_speakers_secure)
+with open('not_secure_names_output.txt', 'w') as f:
+    f.writelines(predicted_speakers_not_secure)
+
+# Get the continuous similarity for every speaker. It amounts to a dot product between the 
+# embedding of the speaker and the continuous embedding of the interview
+#similarity_dict = {name: cont_embeds @ speaker_embed for name, speaker_embed in 
+#                   zip(speaker_names, speaker_embeds)}
+
+
+## Run the interactive demo
+#interactive_diarization(similarity_dict, wav, wav_splits)
